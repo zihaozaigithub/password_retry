4828248381da1fcbd3b0f86321cf2922eed91f34
Leo Li <Lizihaomail@gmail.com>
2022-03-09 16:47:37 -0800

    updated password_retry

--- password_retry.py
+++ password_retry.py
@@ -1,6 +1,6 @@
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
 	pwd = input('please enter password:')
 	if pwd == password:
 		print ('log in succeed')
@@ -8,5 +8,5 @@ while True:
 	else:
 		i = i - 1
 		print('invalid password, please retry, you have', i, 'attemps left' )
-		if i == 0:
-			break
+	
+		
